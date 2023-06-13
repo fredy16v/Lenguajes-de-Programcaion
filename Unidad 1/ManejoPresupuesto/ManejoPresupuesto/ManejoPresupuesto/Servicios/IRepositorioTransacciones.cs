6fbab7acb580dc58ed6c0e7fab45df7086a849c5
fredy16v <fredy16v@gmail.com>
2023-06-12 19:59:08 -0600

    HOLA

--- Unidad 1/ManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Servicios/IRepositorioTransacciones.cs
+++ Unidad 1/ManejoPresupuesto/ManejoPresupuesto/ManejoPresupuesto/Servicios/IRepositorioTransacciones.cs
@@ -1,6 +1,9 @@
-﻿namespace ManejoPresupuesto.Servicios
+﻿using ManejoPresupuesto.Models;
+
+namespace ManejoPresupuesto.Servicios
 {
 	public interface IRepositorioTransacciones
 	{
+		Task Crear(Transaccion transaccion);
 	}
 }
