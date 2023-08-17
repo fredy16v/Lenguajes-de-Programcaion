f9a1cc8e84610fcbb06b6a5046ecdbe72aa8de4e
fredy16v <fredy16v@gmail.com>
2023-08-16 19:49:57 -0600

    para que lo arregle

---
+++ Unidad 2/TareasMVC/Models/RegistroViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TareasMVC.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        [Display(Name = "Correo")]
+        [EmailAddress(ErrorMessage = "Debe colocar un correo valido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La {0} es obligatoria")]
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar Contraseña")]
+        [Compare(nameof(Password), ErrorMessage = "Las contraseñas no coinciden")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
