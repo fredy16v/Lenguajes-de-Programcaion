4385d2fac30f4a58c1ade58f62c86760be7b7586
fredy16v <fredy16v@gmail.com>
2023-08-16 20:40:22 -0600

    todo finalizado

--- Unidad 2/TareasMVC/Controllers/UsuariosController.cs
+++ Unidad 2/TareasMVC/Controllers/UsuariosController.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
 using TareasMVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace TareasMVC.Controllers;
 
@@ -61,5 +64,34 @@ public class UsuariosController : Controller
     public IActionResult Login()
     {
         return View();
+    }
+    
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel modelo)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(modelo);
+        }
+
+        var resultado = await signInManager.PasswordSignInAsync(modelo.Email, modelo.Password,
+            modelo.RememberMe, lockoutOnFailure: false);
+        if (resultado.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos");
+            return View(modelo);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+        return RedirectToAction("Index", "Home");
     }
 }
